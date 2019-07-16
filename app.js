231b09c5e02095d7fab83b2d69fd8004bdad8cd6
carrych <romkacarry@gmail.com>
2019-07-16 22:27:02 +0300

    Skeleton of the project

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('./configs/mongo');
+const bodyParser = require('body-parser');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var adminRouter = require('./routes/admin');
+var contactRouter = require('./routes/contact');
+var postRouter = require('./routes/single-post');
+var successRouter = require('./routes/success');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+//
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/admin', adminRouter);
+app.use('/contact', contactRouter);
+app.use('/single-post', postRouter);
+app.use('/success', successRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
